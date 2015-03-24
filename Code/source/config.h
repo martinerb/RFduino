78e8d51df6be48ad625ce5df4acc3278f8db101a
Martin Erb <martin.erb@student.tugraz.at>
2015-03-24 11:28:45 +0100

    submission

--- Code/source/config.h
+++ Code/source/config.h
@@ -16,9 +16,9 @@
 //GSM
 #define CLOCK_PIN				 4
 #define TOGGLE_INTERVAL 100
-#define PHONE_NUMBER "00436801135508"//"00436603995135"
-#define HW_SERIAL_RX 5 //muss auf pin7
-#define HW_SERIAL_TX 6// muss auf pin9
+#define PHONE_NUMBER // has to be a string ""
+#define HW_SERIAL_RX 5 //on pin7
+#define HW_SERIAL_TX 6// on pin9
 #define HW_BAUDRATE 9600
 
 
